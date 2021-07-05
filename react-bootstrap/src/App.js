76bce96a92b9aaa4618b766ec557f02985df1f98
artiseni <artiseni95@qmail.id>
2021-07-05 10:41:54 +0700

    dom

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -9,21 +9,28 @@ const header = (props) => {
     <div>
       <h1>Hello {props.name}</h1>
       <p>Are you okay?, { props.response }</p>
+      <h3 id="text">{' '}</h3>
     </div>
   )
 }
 
-const App = () => {
 
+const App = () => {
+  
   const name = "Imin"
   const response = "Yes!"
+  
+  const btnHello = () => {
+    const text = document.getElementById('text')
+    text.innerHTML = name
+  }
 
   return (
     <div className="App">
-    {
+      {
         header({ name, response })
-    }
-    <Button varian="primary">Hello</Button>
+      }
+      <Button varian="primary" onClick={btnHello}>Hello</Button>
     </div>
   )
 }
